59287c31dfe03159351d09749dd6d62cbd7a90dd
Gregory.Johnson <3531op@gmail.com>
2020-12-10 19:33:07 -0500

    Initial commit.

--- main.js
+++ main.js
@@ -1,11 +1,46 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+const Person = (first, last, age = 0, married = false) => {
+  const userInfo = {
 
+    firstName: first,
+    lastName: last,
+    age: age,
+    married: married,
 
+    goingOn: function () {
+      return userInfo.age + 1
 
+    },
+
+    ageUp: function () {
+      return userInfo.age++
+    },
+
+    getFullName: function () {
+      return `${userInfo.firstName} ${userInfo.lastName}`
+    },
+
+    marry: function (person) {
+      this.married = true
+      person.married = true
+      this.spouseName = `${person.firstName} ${person.lastName}`
+      person.spouseName = `${this.firstName} ${this.lastName}`
+    },
+
+    divorce: function (person) {
+      this.married = false
+      person.married = false
+      delete this.spouseName
+      delete person.spouseName
+    },
+    
+  }
+  return userInfo
+}
 /*********************************
- * OUR CODE BELOW; DO NOT TOUCH! *
+ * OUR CODE BELOW; DO NOT TOUCH! YOU'RE NOT MY SUPERVISOR!*
  *********************************/
 
 if (typeof Person === 'undefined') {
